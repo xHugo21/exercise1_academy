9016120164ce072d2fbc90ebe8f1a9da623cdfe4
xHugo21 <hugog@hugog.local>
2023-02-21 20:20:29 +0100

    Modified README and added functionality

--- README.md
+++ README.md
@@ -1,45 +1,22 @@
-[Lesson resume](https://github.com/empathyco/academy-paths/blob/main/Engineering%20Paths/Frontend%20Path/html-css.md)
+# First exercise Academy Frontend Developer at Empathy.co by Hugo García Cuesta
 
-# HTML
+## Execution
+1. Install node.js
+2. Clone repository
+```
+git clone https://github.com/xHugo21/exercise1_academy.git
+```
+3. Run npm start inside the project folder
+```
+cd exercise1_academy
+```
+```
+npm start
+```
 
-[HTML in MDN](https://developer.mozilla.org/en-US/docs/Web/HTML)
+*There are some scripts integrated such as npm run-script lint, npm run-script prettier and npm run-script sass
 
-[Introduction to HTML](https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Getting_started#what_is_html)
+***
 
-[Images in HTML](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Images_in_HTML)
-
-[HTML Elements list](https://developer.mozilla.org/en-US/docs/Web/HTML/Element)
-
-# CSS
-
-[What is CSS](https://developer.mozilla.org/en-US/docs/Learn/CSS/First_steps/What_is_CSS)
-
-[CSS getting started](https://developer.mozilla.org/en-US/docs/Learn/CSS/First_steps/Getting_started)
-
-[CSS dinner](http://flukeout.github.io/)
-
-## Normal flow
-
-[Normal flow on MDN](https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Normal_Flow)
-
-## Flexbox
-
-[Flexbox in MDN](https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Flexbox)
-
-[Flexbox zombies](https://flexboxzombies.com/p/flexbox-zombies)
-
-[Flexbox froggy](https://flexboxfroggy.com)
-
-## Grid
-
-[Grid in MDN](https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Grids)
-
-[Grid garden](https://cssgridgarden.com/)
-
-## References
-
-[MDN](https://developer.mozilla.org/en-US/)
-
-[Can I use?](https://caniuse.com/)
-
-[CSS-tricks](https://css-tricks.com/)
+## Funtionality
+This interactive webpage allows the user to search for any characters from the Rick and Morty show by name and status. The characters are fetched from this [api](https://rickandmortyapi.com/)
--- www/js/src.js
+++ www/js/src.js
@@ -1,32 +1,51 @@
+// Triggered when search bar is updated
 function search_update(){
     //event_fired = true;
     const search_value = search_bar.value;
-
-    get_results(search_value, "alive");
+    console.log(search_bar.value);
+    update_results(search_value, "alive");
 }
 
-const get_results = async(name, status) => {
+// Updates search results based on name and status
+const update_results = async(name, status) => {
+    // In case there is nothing on the search bar, display all characters
+    if (name === ""){
+        var url = "https://rickandmortyapi.com/api/character";
+    }
     // Get matching results from api
-    const url = "https://rickandmortyapi.com/api/character/?name="+name+"&status="+status;
+    else{
+        var url = "https://rickandmortyapi.com/api/character/?name="+name+"&status="+status;
+    }
+    // Wait for api response
     const api = await fetch(url);
     const data = await api.json();
     console.log(data);
 
+    // Remove previous results
     const target_div = document.querySelector(".search_results");
-    target_div.childNodes.forEach(element => target_div.removeChild(element))
+    target_div.innerHTML = "";
+    
     // Insert results as article into search_results div
     data.results.forEach(element => {
         const newArticle = document.createElement("article");
-        newArticle.innerHTML = "<img src="+element.image+"> <div class=result_footer><h4>"+element.name+"</h4> <p>"+element.status+"</p>";
+        newArticle.setAttribute("class", "article_result");
+        newArticle.innerHTML = "<img src="+element.image+"> <div class=result_footer><h4>"+element.name+"</h4> <p class="+element.status.toLowerCase()+">"+element.status+"</p>";
         //const target_div = document.querySelector(".search_results");
         target_div.append(newArticle);
         
     })
 }
 
+// Main flow of the program
+// Set default search
+update_results("", "alive")
 
+// Identify search bar and add EventListener
 const search_bar = document.querySelector(".search_bar");
-console.log(search_bar);
-search_bar.addEventListener("keypress", search_update);
+search_bar.addEventListener("keyup", search_update);
+
+// Identify checkboxes and add EventListener
+const checkbox = document.getElementsByName("checkbox");
+
 //get_results("rick", "alive");
 
