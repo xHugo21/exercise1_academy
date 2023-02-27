90f05f8032623b1b6b9318ca3e4033886aec2f48
xHugo21 <hugog@hugog.local>
2023-02-27 12:03:57 +0100

    Debounce y reload con boton

--- www/js/src.js
+++ www/js/src.js
@@ -1,8 +1,21 @@
+//VARIABLES
+let data;
+
 // FUNCTIONS
+// Debounce
+function debounce(func, timeout=300){
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this); }, timeout);
+    }; 
+}
+
 // Triggered when search bar or checkboxes are triggered
-function search_update(search_bar, checkbox, page){
+function search_update(){
     let status = [];
     const name = search_bar.value;
+    console.log
 
     // Update status array with checked boxes
     checkbox.forEach(element => {
@@ -13,42 +26,42 @@ function search_update(search_bar, checkbox, page){
     })
 
     // Call update_results with name and status parameters
-    update_results(name, status, page);
+    update_results(name, status);
 }
 
+const debounce_change = debounce(() => search_update());
+
 // Updates search results based on name and status
-const update_results = async(name, status, page) => {
+async function update_results(name, status){
     // In case there is nothing on the search bar, display all characters
     var url;
-    if (name == "" && (status.length == 3 || status.length == 0)){
-        url = "https://rickandmortyapi.com/api/character/?page="+page;
+
+    if (status.length == 2){ //TODO
+        console.log("length 2");
+        url = "https://rickandmortyapi.com/api/character/?name="+name+"&status="+status[0]+"&status="+status[1];
     }
 
-    else if (name != "" && (status.length == 3 || status.length == 0)){
-        url = "https://rickandmortyapi.com/api/character/?name="+name+"?page="+page;
+    else if (status.length == 1){
+        console.log("length 1");
+        url = "https://rickandmortyapi.com/api/character/?name="+name+"&status="+status[0];
     }
 
-    else{
-        if (status.length == 1){
-            url = "https://rickandmortyapi.com/api/character/?name="+name+"&status="+status[0];
-        }
-        else if (status.length == 2){ // TODO no funciona
-            url = "https://rickandmortyapi.com/api/character/?name="+name+"&status=["+status[0]+", "+status[1]+"]";
-        }
+    else if (status.length == 3 || status.length == 0){
+        console.log("length 3");    
+        url = "https://rickandmortyapi.com/api/character/?name="+name;
     }
+
 
     console.log(url);
 
     // Wait for api response
     const api = await fetch(url);
-    var data = await api.json();
+    data = await api.json();
     console.log(data);
 
     // Remove previous results
-    if (page == 1){
-        var target_div = document.querySelector(".search_results");
-        target_div.innerHTML = "";
-    }
+    var target_div = document.querySelector(".search_results");
+    target_div.innerHTML = "";
     
     // Insert results as article into search_results div
     data.results.forEach(element => {
@@ -59,9 +72,31 @@ const update_results = async(name, status, page) => {
     })
 }
 
+async function load_more_results(){
+    if (data.info.next == null){
+        console.log("THERE AREN'T MORE RESULTS TO DISPLAY");
+        return;
+    }
+
+    let url = data.info.next;
+
+    const api = await fetch(url);
+    data = await api.json();
+    console.log(data);
+
+    var target_div = document.querySelector(".search_results");
+
+    data.results.forEach(element => {
+        const newArticle = document.createElement("article");
+        newArticle.setAttribute("class", "article_result");
+        newArticle.innerHTML = "<img src="+element.image+"> <div class=result_footer><h4>"+element.name+"</h4> <p class="+element.status.toLowerCase()+">"+element.status+"</p>";
+        target_div.append(newArticle);
+    })
+}
+
 // MAIN FLOW OF THE PROGRAM
 // Set initial results
-update_results("", ["alive", "dead", "unknown"], 1);
+update_results("", ["alive", "dead", "unknown"]);
 
 // Get checkboxes
 const checkbox = document.getElementsByName("checkbox");
@@ -69,22 +104,25 @@ const checkbox = document.getElementsByName("checkbox");
 // Get search bar
 const search_bar = document.querySelector(".search_bar");
 
-// Event listeners for search bar and checkboxes
-search_bar.addEventListener("keyup", function(){search_update(search_bar, checkbox, 1);});
-checkbox.forEach(element=>{
-    element.addEventListener("click", function(){search_update(search_bar, checkbox, 1);});
-})
+const loadmore = document.querySelector(".loadmore");
 
-document.addEventListener('DOMContentLoaded', function(e) {
-    document.addEventListener('scroll', function(e) {
-        let documentHeight = document.body.scrollHeight;
-        let currentScroll = window.scrollY + window.innerHeight;
-        // When the user is [modifier]px from the bottom, fire the event.
-        let modifier = 200; 
-        if(currentScroll + modifier > documentHeight) {
-            update_results(search_bar, checkbox, 2);
-        }
-    })
+// Event listeners for search bar and checkboxes
+search_bar.addEventListener("keyup", function(){debounce_change();});
+checkbox.forEach(element=>{
+    element.addEventListener("click", function(){debounce_change();});
 })
+loadmore.addEventListener("click", function(){load_more_results();});
+
+//document.addEventListener('DOMContentLoaded', function(e) {
+//    document.addEventListener('scroll', function(e) {
+//        let documentHeight = document.body.scrollHeight;
+//        let currentScroll = window.scrollY + window.innerHeight;
+//        // When the user is [modifier]px from the bottom, fire the event.
+//        let modifier = 200; 
+//        if(currentScroll + modifier > documentHeight) {
+//            update_results(search_bar, checkbox, 2);
+//        }
+//    })
+//})
 
 
